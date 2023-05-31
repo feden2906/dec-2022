ae1a89c9b2ab77db010a98b6ea9fe851e3b75cfb
l4pukh <k0styalopuh3@gmail.com>
2023-05-31 21:58:47 +0300

    events, streams, express

--- app.js
+++ app.js
@@ -1,73 +1,135 @@
-// MODULES //
-// const { sayHello } = require('./helpers/sayHello.helper');
-// sayHello();
-
-// GLOBAL VARIABLES //
-//__dirname, __filename, process.cwd();
-// console.log('from app.js');
+// EVENTS
+// const events = require('node:events');
 //
-// console.log(__dirname);
-// console.log(__filename);
-// console.log(process.cwd());
-// CWD - current working directory
-
-// PATH
-// const path = require('path');
-// C:\Users\l4pukhh\WebstormProjects\dec-2022
-// Users/l4pukhh/WebstormProjects/dec-2022/helpers
-// const joinedPath = path.join(__dirname, 'folder', 'folder2', 'text.txt');
-// const normalizedPath = path.normalize('////test////test2////////test3///test4');
-// const resolvedPath = path.resolve('folder', 'folder2', 'text.txt');
+// const eventEmitter = new events();
 //
-// console.log(joinedPath)
-// console.log(normalizedPath);
-// console.log(resolvedPath);
-
-// OS
-// const os = require('os');
-// const { exec } = require('child_process');
+// eventEmitter.on('click', (data)=>{
+//   console.log(data)
+//   console.log('Click click click');
+// })
 //
-// // x64 x86 = x32
-// console.log(os.cpus());
-// console.log(os.arch());
-// console.log(os.version());
-
+// eventEmitter.emit('click', { data: "Hello" });
+// eventEmitter.emit('click');
+// eventEmitter.emit('click');
+// eventEmitter.emit('click');
+// eventEmitter.emit('click');
+//
+// console.log(eventEmitter.eventNames());
+//
+// eventEmitter.once('clickAndDie', ()=>{
+//   console.log('Clicked and died');
+// })
+//
+// console.log(eventEmitter.eventNames());
+//
+// eventEmitter.emit('clickAndDie');
+// eventEmitter.emit('clickAndDie');
+// eventEmitter.emit('clickAndDie');
+// eventEmitter.emit('clickAndDie');
+//
+// console.log(eventEmitter.eventNames());
+//
 // const fs = require('fs');
-// const path = require('path')
 //
-// const text2Path = path.join(__dirname, 'folder', 'folder2', 'text2.txt');
-
-// fs.writeFile(text2Path, 'Hello from Okten again', (err)=>{
-//   if (err) throw new Error(err.message);
+// const readStream = fs.createReadStream('text.txt');
+// const writeStream = fs.createWriteStream('text2.txt');
+//
+// readStream.on('data', (chunk)=>{
+//   console.log(chunk);
+//   writeStream.write(chunk)
 // })
 //
-// fs.readFile(text2Path, { encoding: 'utf-8' },(err, data)=>{
-//   if (err) throw new Error(err.message);
-//   console.log(data);
-// })
+// readStream
+//   .on('error', ()=>{
+//     readStream.destroy();
+//     writeStream.end('ERROR ON READING FILE');
 //
-// fs.appendFile(text2Path, '\njsdkalf;sdjak;flda', (err)=>{
-//   if (err) throw new Error(err.message);
-// })
-//
-// fs.truncate(text2Path, (err)=>{
-//   if (err) throw new Error(err.message);
-// })
-//
-// fs.unlink(text2Path, (err)=>{
-//   if (err) throw new Error(err.message);
-// })
-//
-// fs.readdir(path.join(__dirname, 'folder'), { withFileTypes: true }, (err, files)=>{
-//   if (err) throw new Error(err.message);
-//   files.forEach(file=>{
-//     console.log(file.isDirectory());
+//     // handle error
 //   })
-// })
+//   .pipe(writeStream)
 //
-// fs.mkdir(path.join(__dirname, 'folder', 'folder4'), (err)=>{
-//   if (err) throw new Error(err.message);
-// })
-// fs.rm(path.join(__dirname, 'folder', 'folder2'), { recursive: true }, (err)=>{
-//   if (err) throw new Error(err.message);
-// })
+// read, write, duplex, transform - !!!
+
+const express = require('express')
+
+const users = [
+  {
+    name: 'Oleh',
+    age: 20,
+    gender: 'male'
+  },
+  {
+    name: 'Anton',
+    age: 10,
+    gender: 'male'
+  },
+  {
+    name: 'Inokentiy',
+    age: 25,
+    gender: 'female'
+  },
+  {
+    name: 'Anastasiya',
+    age: 15,
+    gender: 'female'
+  },
+  {
+    name: 'Cocos',
+    age: 25,
+    gender: 'other',
+  },
+]
+
+const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
+// CRUD - create, read, update, delete
+
+app.get('/users', (req, res) => {
+  res.status(200).json(users)
+})
+
+app.get('/users/:id', (req, res) => {
+  const { id } = req.params;
+
+  res.status(200).json(users[+id]);
+})
+
+app.post('/users', (req, res)=>{
+  users.push(req.body);
+
+  res.status(201).json({
+    message: "User created."
+  });
+})
+
+app.put('/users/:id', (req, res)=>{
+  const { id } = req.params;
+
+  users[+id] = req.body;
+
+  res.status(200).json({
+    message: 'User updated',
+    data: users[+id],
+  })
+})
+
+app.delete('/users/:id', (req, res)=>{
+  const { id } = req.params;
+
+  users.splice(+id, 1);
+
+  res.status(200).json({
+    message: 'User deleted',
+  })
+})
+
+const PORT = 5001;
+
+app.listen(PORT, () => {
+  console.log(`Server has started on PORT ${PORT} 🥸`)
+})
+
