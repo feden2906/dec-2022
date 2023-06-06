0b817fdc518b2c8e3eb0d06ed0fbc402cb7609d7
feden2906 <feden2906@gmail.com>
2023-06-06 21:04:00 +0300

    [hw4] joi validator / error handler

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import * as mongoose from "mongoose";
 
 import { configs } from "./configs/config";
+import { ApiError } from "./errors";
 import { User } from "./models/User.mode";
 import { IUser } from "./types/user.type";
+import { UserValidator } from "./validators";
 
 const app = express();
 
@@ -25,6 +27,27 @@ app.get(
   }
 );
 
+app.post(
+  "/users",
+  async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response<IUser>> => {
+    try {
+      const { error, value } = UserValidator.create.validate(req.body);
+      if (error) {
+        throw new ApiError(error.message, 400);
+      }
+      const createdUser = await User.create(value);
+
+      return res.status(201).json(createdUser);
+    } catch (e) {
+      next(e);
+    }
+  }
+);
+
 app.get(
   "/users/:id",
   async (req: Request, res: Response): Promise<Response<IUser>> => {
@@ -38,33 +61,29 @@ app.get(
   }
 );
 
-app.post(
-  "/users",
-  async (req: Request, res: Response): Promise<Response<IUser>> => {
-    try {
-      const createdUser = await User.create(req.body);
-
-      return res.status(201).json(createdUser);
-    } catch (e) {
-      console.log(e);
-    }
-  }
-);
-
 app.put(
   "/users/:id",
-  async (req: Request, res: Response): Promise<Response<IUser>> => {
+  async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response<IUser>> => {
     try {
       const { id } = req.params;
+
+      const { error, value } = UserValidator.update.validate(req.body);
+      if (error) {
+        throw new ApiError(error.message, 400);
+      }
       const updatedUser = await User.findOneAndUpdate(
         { _id: id },
-        { ...req.body },
+        { ...value },
         { returnDocument: "after" }
       );
 
       return res.status(200).json(updatedUser);
     } catch (e) {
-      console.log(e);
+      next(e);
     }
   }
 );
@@ -82,6 +101,11 @@ app.delete(
     }
   }
 );
+
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+  const status = error.status || 500;
+  return res.status(status).json(error.message);
+});
 
 app.listen(configs.PORT, () => {
   mongoose.connect(configs.DB_URL);
