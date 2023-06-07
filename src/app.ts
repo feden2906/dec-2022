4858e3ce5a1c796aa8e74620c2e3bbdb6af17b5e
l4pukh <k0styalopuh3@gmail.com>
2023-06-07 22:10:58 +0300

    structure

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,7 @@ import * as mongoose from "mongoose";
 
 import { configs } from "./configs/config";
 import { ApiError } from "./errors";
-import { User } from "./models/User.mode";
-import { IUser } from "./types/user.type";
-import { UserValidator } from "./validators";
+import { userRouter } from "./routers/user.router";
 
 const app = express();
 
@@ -14,97 +12,15 @@ app.use(express.urlencoded({ extended: true }));
 
 // CRUD - create, read, update, delete
 
-app.get(
-  "/users",
-  async (req: Request, res: Response): Promise<Response<IUser[]>> => {
-    try {
-      const users = await User.find().select("-password");
+app.use("/users", userRouter);
 
-      return res.json(users);
-    } catch (e) {
-      console.log(e);
-    }
-  }
-);
+app.use((err: ApiError, req: Request, res: Response, next: NextFunction) => {
+  const status = err.status || 500;
 
-app.post(
-  "/users",
-  async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ): Promise<Response<IUser>> => {
-    try {
-      const { error, value } = UserValidator.create.validate(req.body);
-      if (error) {
-        throw new ApiError(error.message, 400);
-      }
-      const createdUser = await User.create(value);
-
-      return res.status(201).json(createdUser);
-    } catch (e) {
-      next(e);
-    }
-  }
-);
-
-app.get(
-  "/users/:id",
-  async (req: Request, res: Response): Promise<Response<IUser>> => {
-    try {
-      const user = await User.findById(req.params.id);
-
-      return res.json(user);
-    } catch (e) {
-      console.log(e);
-    }
-  }
-);
-
-app.put(
-  "/users/:id",
-  async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ): Promise<Response<IUser>> => {
-    try {
-      const { id } = req.params;
-
-      const { error, value } = UserValidator.update.validate(req.body);
-      if (error) {
-        throw new ApiError(error.message, 400);
-      }
-      const updatedUser = await User.findOneAndUpdate(
-        { _id: id },
-        { ...value },
-        { returnDocument: "after" }
-      );
-
-      return res.status(200).json(updatedUser);
-    } catch (e) {
-      next(e);
-    }
-  }
-);
-
-app.delete(
-  "/users/:id",
-  async (req: Request, res: Response): Promise<Response<void>> => {
-    try {
-      const { id } = req.params;
-      await User.deleteOne({ _id: id });
-
-      return res.sendStatus(200);
-    } catch (e) {
-      console.log(e);
-    }
-  }
-);
-
-app.use((error: any, req: Request, res: Response, next: NextFunction) => {
-  const status = error.status || 500;
-  return res.status(status).json(error.message);
+  return res.status(status).json({
+    message: err.message,
+    status: err.status,
+  });
 });
 
 app.listen(configs.PORT, () => {
--- src/types/user.type.ts
+++ src/types/user.type.ts
@@ -2,9 +2,9 @@ import { Types } from "mongoose";
 
 export interface IUser {
   _id: Types.ObjectId;
-  name: string;
-  age: number;
-  gender: string;
+  name?: string;
+  age?: number;
+  gender?: string;
   email: string;
   password: string;
 }
