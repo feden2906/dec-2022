e91f8eabe0ce4296909222d439abc43150fa0938
feden2906 <feden2906@gmail.com>
2023-06-08 20:47:47 +0300

    [hw5] dynamic middleware / finish structure

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,10 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 
-import { ApiError } from "../errors";
-import { User } from "../models/User.mode";
 import { userService } from "../services/user.service";
 import { IUser } from "../types/user.type";
-import { UserValidator } from "../validators";
 
 class UserController {
   public async findAll(
@@ -27,7 +24,7 @@ class UserController {
     next: NextFunction
   ): Promise<Response<IUser>> {
     try {
-      const createdUser = await userService.create(req.res.locals as IUser);
+      const createdUser = await userService.create(req.body);
 
       return res.status(201).json(createdUser);
     } catch (e) {
@@ -41,7 +38,9 @@ class UserController {
     next: NextFunction
   ): Promise<Response<IUser>> {
     try {
-      const user = await userService.findById(req.params.id);
+      const { userId } = req.params;
+
+      const user = await userService.findById(userId);
 
       return res.json(user);
     } catch (e) {
@@ -55,17 +54,9 @@ class UserController {
     next: NextFunction
   ): Promise<Response<IUser>> {
     try {
-      const { id } = req.params;
+      const { userId } = req.params;
 
-      const { error, value } = UserValidator.update.validate(req.body);
-      if (error) {
-        throw new ApiError(error.message, 400);
-      }
-      const updatedUser = await User.findOneAndUpdate(
-        { _id: id },
-        { ...value },
-        { returnDocument: "after" }
-      );
+      const updatedUser = await userService.updateById(userId, req.body);
 
       return res.status(200).json(updatedUser);
     } catch (e) {
@@ -79,10 +70,11 @@ class UserController {
     next: NextFunction
   ): Promise<Response<void>> {
     try {
-      const { id } = req.params;
-      await User.deleteOne({ _id: id });
+      const { userId } = req.params;
 
-      return res.sendStatus(200);
+      await userService.deleteById(userId);
+
+      return res.sendStatus(204);
     } catch (e) {
       next(e);
     }
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -5,19 +5,39 @@ import { IUser } from "../types/user.type";
 
 class UserService {
   public async findAll(): Promise<IUser[]> {
-    try {
-      return User.find().select("-password");
-    } catch (e) {
-      throw new ApiError(e.message, e.status);
-    }
+    return await User.find();
   }
 
   public async create(data: IUser): Promise<IUser> {
-    return userRepository.create(data);
+    return await userRepository.create(data);
   }
 
   public async findById(id: string): Promise<IUser> {
-    return User.findById(id);
+    return await this.getOneByIdOrThrow(id);
+  }
+
+  public async updateById(userId: string, dto: Partial<IUser>): Promise<IUser> {
+    await this.getOneByIdOrThrow(userId);
+
+    return await User.findOneAndUpdate(
+      { _id: userId },
+      { ...dto },
+      { returnDocument: "after" }
+    );
+  }
+
+  public async deleteById(userId: string): Promise<void> {
+    await this.getOneByIdOrThrow(userId);
+
+    await User.deleteOne({ _id: userId });
+  }
+
+  private async getOneByIdOrThrow(userId: string): Promise<IUser> {
+    const user = await User.findById(userId);
+    if (!user) {
+      throw new ApiError("User not found", 422);
+    }
+    return user;
   }
 }
 
