c9dfcc689957ac75a866c6be4eceb293bef8821b
feden2906 <feden2906@gmail.com>
2023-06-13 21:39:10 +0300

    [hw6] check access token middleware / login validation / uniq user by email middleware

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -22,9 +22,6 @@ class AuthService {
     user: IUser
   ): Promise<ITokensPair> {
     try {
-      //TODO: remove, settle inside of the middleware
-      user = await User.findOne({ email: credentials.email });
-
       const isMatched = await passwordService.compare(
         credentials.password,
         user.password
@@ -35,6 +32,7 @@ class AuthService {
 
       const tokensPair = await tokenService.generateTokenPair({
         _id: user._id,
+        name: user.name,
       });
 
       await Token.create({
--- src/validators/user.validator.ts
+++ src/validators/user.validator.ts
@@ -30,4 +30,9 @@ export class UserValidator {
     age: this.age,
     gender: this.gender,
   });
+
+  static login = Joi.object({
+    email: this.email.required(),
+    password: this.password.required(),
+  });
 }
