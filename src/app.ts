caa6e024e14d5ac57db8d0b2fb7fef000eb657c5
l4pukh <k0styalopuh3@gmail.com>
2023-06-26 21:44:05 +0300

    cron, swagger

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express, { NextFunction, Request, Response } from "express";
 import * as mongoose from "mongoose";
+import * as swaggerUi from "swagger-ui-express";
 
 import { configs } from "./configs/config";
+import { cronRunner } from "./crons";
 import { ApiError } from "./errors";
 import { authRouter } from "./routers/auth.router";
 import { userRouter } from "./routers/user.router";
+import * as swaggerJson from "./utils/swagger.json";
 
 const app = express();
 
@@ -15,6 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/users", userRouter);
 app.use("/auth", authRouter);
+app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerJson));
 
 app.use((err: ApiError, req: Request, res: Response, next: NextFunction) => {
   const status = err.status || 500;
@@ -27,5 +31,6 @@ app.use((err: ApiError, req: Request, res: Response, next: NextFunction) => {
 
 app.listen(configs.PORT, () => {
   mongoose.connect(configs.DB_URL);
+  cronRunner();
   console.log(`Server has started on PORT ${configs.PORT} 🥸`);
 });
--- src/models/Token.model.ts
+++ src/models/Token.model.ts
@@ -2,20 +2,23 @@ import { model, Schema, Types } from "mongoose";
 
 import { User } from "./User.mode";
 
-const tokensSchema = new Schema({
-  accessToken: {
-    type: String,
-    required: true,
+const tokensSchema = new Schema(
+  {
+    accessToken: {
+      type: String,
+      required: true,
+    },
+    refreshToken: {
+      type: String,
+      required: true,
+    },
+    _userId: {
+      type: Types.ObjectId,
+      required: true,
+      ref: User,
+    },
   },
-  refreshToken: {
-    type: String,
-    required: true,
-  },
-  _userId: {
-    type: Types.ObjectId,
-    required: true,
-    ref: User,
-  },
-});
+  { versionKey: false, timestamps: true }
+);
 
 export const Token = model("token", tokensSchema);
