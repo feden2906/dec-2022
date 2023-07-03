b3c8b1f7e58a3cb2a3d12ae3526367ec4ca61c85
feden2906 <feden2906@gmail.com>
2023-07-03 23:45:00 +0300

    format

--- src/services/sms.service.ts
+++ src/services/sms.service.ts
@@ -1,25 +1,29 @@
-import {Twilio} from 'twilio'
-import {configs} from "../configs/config";
-import {ESmsActions} from "../enums/sms.enum";
-import {smsTemplates} from "../constants/sms.constants";
+import { Twilio } from "twilio";
+
+import { configs } from "../configs/config";
+import { smsTemplates } from "../constants/sms.constants";
+import { ESmsActions } from "../enums/sms.enum";
 
 class SmsService {
-    constructor(private client = new Twilio(configs.TWILIO_ACCOUNT_SID, configs.TWILIO_TOKEN)) {
-    }
+  constructor(
+    private client = new Twilio(
+      configs.TWILIO_ACCOUNT_SID,
+      configs.TWILIO_TOKEN
+    )
+  ) {}
 
-    public async sendSms(phone: string, action: ESmsActions) {
-        try {
-            const template = smsTemplates[action];
-            await this.client.messages.create({
-                body: template,
-                messagingServiceSid: configs.TWILIO_SERVICE_SID,
-                to: phone,
-            });
-
-        } catch (e) {
-            console.error(e.message);
-        }
+  public async sendSms(phone: string, action: ESmsActions) {
+    try {
+      const template = smsTemplates[action];
+      await this.client.messages.create({
+        body: template,
+        messagingServiceSid: configs.TWILIO_SERVICE_SID,
+        to: phone,
+      });
+    } catch (e) {
+      console.error(e.message);
     }
+  }
 }
 
 export const smsService = new SmsService();
